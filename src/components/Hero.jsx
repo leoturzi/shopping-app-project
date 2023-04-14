9078f574c851dd418088bc6e35d8369b0d6b06bd
Leo Turzi <leogturzi@gmail.com>
2023-04-14 03:18:36 -0600

    Rename data file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
-import { sliderItems } from '../sliderData';
+import { sliderItems } from '../data';
 
 const Container = styled.div`
     width: 100%;
